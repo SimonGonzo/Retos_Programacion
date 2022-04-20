608ca590f755853ac026ad417e56564a68ed7f8a
SimonGonzo <103767567+SimonGonzo@users.noreply.github.com>
2022-04-20 10:56:45 -0500

    Update reto5.js
    change all code for the final execution code.

--- reto5.js
+++ reto5.js
@@ -1,19 +1,12 @@
-let today = new Date('Jan 1, 2022 00:00:01');
-let christmasYear = 2021;
+export default function daysToXmas(date) {
+  let Fechaactual=new Date(date);
+  let navidadAnoActual=2021;
 
-//if (today.getMonth() == 11 && today.getDate() > 25) {
- // christmasYear = christmasYear + 1;
-//}
-
-let christmasDate = new Date(christmasYear, 11, 25);
-let dayMilliseconds = 1000 * 60 * 60 * 24;
-if (christmasYear>2021) {
-
+  let FechaNavidad = new Date(navidadAnoActual,11,25);
+  let MilisegundosUnDia= 1000*60*60*24;
+  let DiasFaltantes= Math.ceil(
+    (FechaNavidad.getTime() - Fechaactual.getTime())/
+     (MilisegundosUnDia)
+  );
+  return DiasFaltantes
 }
-
-let remainingDays = Math.ceil(
-  (christmasDate.getTime() - today.getTime()) /
-   (dayMilliseconds)
-);
-
-document.write(remainingDays)
